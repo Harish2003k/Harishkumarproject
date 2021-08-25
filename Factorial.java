927f3c1edbb210c83d27062c257065ac4b5a84d3
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2021-08-26 00:37:51 +0530

    Add files via upload
    first commit

---
+++ Factorial.java
@@ -0,0 +1,10 @@
+public class factorial{
+    public static void main(String args[]){
+        int i,fact=1;
+        int number=6;
+        for(i=1;i<number;i++){
+            fact=fact*i;
+        }
+        System.out.println("Factorial of  "+number+" is:"+fact);
+    }
+}
