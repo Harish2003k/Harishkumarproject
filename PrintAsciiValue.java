e5158f16f98cf3d5d1a186bef48ef3b6d5b45a1c
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2021-08-26 23:25:19 +0530

    Add files via upload
    First commit

---
+++ PrintAsciiValue.java
@@ -0,0 +1,10 @@
+public class PrintAsciiValue{
+    public static void main(String args[]){
+        char ch1='a';
+        char ch2='b';
+        int ascii1=ch1;
+        int ascii2=ch2;
+     System.out.println("\n The ASCII value of :" +ch1+ " is :"+ascii1);
+     System.out.println("The ASCII value of :" +ch2+ " is: "+ascii2);
+    }
+}
