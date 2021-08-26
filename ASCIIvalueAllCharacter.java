d45a043ab54ae483c53e63fd3662b3d09bcccdcd
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2021-08-27 00:25:48 +0530

    Add files via upload
    First commit

---
+++ ASCIIvalueAllCharacter.java
@@ -0,0 +1,7 @@
+public class AsciiValueOfAllCharacter{
+    public static void main(String args[]){
+        for(int i=0;i<=255;i++){
+            System.out.println("\n The ASCII Value of "+(char)i+"= "+i);
+        }
+    }
+}
