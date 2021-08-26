8cbf449aaa9278d2d00e9fdefee99d373850e11d
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2021-08-26 23:04:25 +0530

    Add files via upload
    First commit

---
+++ CreateObject.java
@@ -0,0 +1,8 @@
+public class CreateObject{
+    CreateObject(){
+        System.out.println("Welcome to BBDU");
+    }
+    public static void main(String args[]){
+        CreateObject obj=new CreateObject();
+    }
+}
