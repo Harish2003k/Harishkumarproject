c890ce37d8a7948ba145c51829f289ccdb1bba1d
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2021-08-28 00:42:36 +0530

    Add files via upload
    First commit

---
+++ IncreasingNumberPattern.java
@@ -0,0 +1,11 @@
+public class IncreasingNumber{
+    public static void main(String args[]){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(""+i);
+            }
+            System.out.println();
+        }
+    }
+}
