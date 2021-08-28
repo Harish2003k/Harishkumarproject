b19426e6290759b9014e196ed58d7e550deded3d
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2021-08-29 00:31:50 +0530

    Add files via upload
    First commit

---
+++ FibonaciiSeries.java
@@ -0,0 +1,12 @@
+public class Fibonacii{
+    public static void main(String args[]){
+        int n1=0,n2=1,n3,i,count=15;
+        System.out.println(n1+""+n2);
+        for(i=2;i<count;i++){
+            n3=n1+n2;
+            System.out.print("\n"+n3);
+            n1=n2;
+            n2=n3;
+        }
+    }
+}
