51f3537df308d35cc85115d214d72bc4855b6e44
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2021-08-29 20:41:07 +0530

    Add files via upload
    First commit

---
+++ LeftShift.java
@@ -0,0 +1,9 @@
+public class ByteShift{
+    public static void main(String args[]){
+    int a=64;
+    int i;
+    i = a<<2;
+    System.out.println("Original value of a:" +a);
+    System.out.println("i and a:" +i+ "");
+    }
+}
