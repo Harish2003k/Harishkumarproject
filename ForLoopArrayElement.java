ebf1552faf4e63c7d17cd1000afed2c9c7faaef1
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2021-09-01 01:44:15 +0530

    Add files via upload
    First commit

---
+++ ForLoopArrayElement.java
@@ -0,0 +1,8 @@
+ public class ForLoop{
+     public static void main(String args[]){
+         int arr[]={2,3,11,45,47,88};
+         for(int i=0;i<arr.length;i++){
+             System.out.print("\nArray of list:"+arr[i]);
+         }
+     }
+ }
