afdb371420b8e0f8fc115b872d6a86577d9c9134
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2021-09-05 18:02:40 +0530

    Add files via upload
    First commit

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,13 +1,13 @@
-// Given Number to be reversed number like this 123 to 321.
 public class ReverseNumber{
-    public static void main(String args[]){
-        int number=987654,reverse=0;
-        while(number!=0){
-            int remainder=number%10;
-            reverse=reverse*10+remainder;
-            number=number/10;
-        }
-        System.out.println("The reverse of given number is:"+reverse);
-    } 
-}
-
+public static void main(String[] args)   
+{  
+int number = 543210, reverse = 0;  
+while(number != 0)   
+{  
+int remainder = number % 10;  
+reverse = reverse * 10 + remainder;  
+number = number/10;  
+}  
+System.out.println("The reverse of the given number is: " + reverse);  
+}  
+}  
